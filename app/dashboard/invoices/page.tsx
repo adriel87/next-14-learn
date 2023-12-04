38c47f4b3563a9de4dc1e370edb90244a5c8f388
adriel <adrielarochaoronoz@gmail.com>
2023-12-04 10:27:02 +0000

    first commit going to database tutorial

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+
+const Page = ()=>{
+    return <p>Invoce Page</p>
+}
+
+export default Page;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental:{
+        typedRoutes: true,
+    },
+
+    // interesante saberlo
+    typescript:{
+         // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        // ignoreBuildErrors: true,
+    }
+};
 
 module.exports = nextConfig;
